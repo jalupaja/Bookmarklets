61939be77b6fd5e59feb5e6a06a2d78fa8d3e32f
jalupa <jalupa@jalupa.fake>
2022-08-25 15:47:12 +0200

    changes to replaceWithPrivacyFriendly(Onion)Alternatives:     added quora, reuters     fixed wikipedia routing

--- Bookmarklets/replaceWithPrivacyFriendlyOnionAlternatives.js
+++ Bookmarklets/replaceWithPrivacyFriendlyOnionAlternatives.js
@@ -1,20 +1,39 @@
 javascript: (function()
 {
-    if (window.location.toString().includes("youtube"))
-        window.location = window.location.toString().replace("https://www.youtube.com", "http://c7hqkpkpemu6e7emz5b4vyz7idjgdvgaaa3dyimmeojqbgpea3xqjoid.onion");
-    else if (window.location.toString().includes("youtu.be"))
-        window.location = window.location.toString().replace("https://youtu.be", "http://c7hqkpkpemu6e7emz5b4vyz7idjgdvgaaa3dyimmeojqbgpea3xqjoid.onion");
-    else if (window.location.toString().includes("twitter"))
-        window.location = window.location.toString().replace("https://twitter.com", "http://nitter7bryz3jv7e3uekphigvmoyoem4al3fynerxkj22dmoxoq553qd.onion");
-    else if (window.location.toString().includes("reddit"))
-        window.location = window.location.toString().replace("https://www.reddit.com", "http://qtpvyiaqhmwccxwzsqubd23xhmmrt75tdyw35kp43w4hvamsgl3x27ad.onion");
-    else if (window.location.toString().includes("instagram"))
-        window.location = window.location.toString().replace("https://www.instagram.com", "http://rlp5gt4d7dtkok3yaogocbcvrs2tdligjrxipsamztjq4wwpxzjeuxqd.onion");
-    else if (window.location.toString().includes("tiktok.com"))
-        window.location=window.location.toString().replace('tiktok.com', 'proxitok.herokuapp.com');
-    else if (window.location.toString().includes("wikipedia"))
-        window.location = window.location.toString().replace(/https:\/\/.?.?\.?wikipedia.org/,'https://wikiless.org');
-    else if (window.location.toString().includes("imgur"))
-        window.location=window.location.toString().replace('https://imgur.com','https://rimgo.bcow.xyz');
+    var win_loc = window.location.toString();
+    if (win_loc.includes("youtube")) {
+        win_loc = win_loc.replace("https://www.youtube.com", "http://c7hqkpkpemu6e7emz5b4vyz7idjgdvgaaa3dyimmeojqbgpea3xqjoid.onion");
+    } else if (win_loc.includes("youtu.be")) {
+        win_loc = win_loc.replace("https://youtu.be", "http://c7hqkpkpemu6e7emz5b4vyz7idjgdvgaaa3dyimmeojqbgpea3xqjoid.onion");
+    } else if (win_loc.includes("twitter")) {
+        win_loc = win_loc.replace("https://twitter.com", "http://nitter7bryz3jv7e3uekphigvmoyoem4al3fynerxkj22dmoxoq553qd.onion");
+    } else if (win_loc.includes("reddit")) {
+        win_loc = win_loc.replace("https://www.reddit.com", "http://qtpvyiaqhmwccxwzsqubd23xhmmrt75tdyw35kp43w4hvamsgl3x27ad.onion");
+    } else if (win_loc.includes("instagram")) {
+        win_loc = win_loc.replace("https://www.instagram.com", "http://rlp5gt4d7dtkok3yaogocbcvrs2tdligjrxipsamztjq4wwpxzjeuxqd.onion");
+    } else if (win_loc.includes("tiktok.com")) {
+        win_loc =win_loc.replace('tiktok.com', 'proxitok.herokuapp.com');
+    } else if (win_loc.includes("wikipedia")) {
+        if (win_loc.includes("www.wikipedia.org") || win_loc.includes("en.wikipedia.org"))
+            var lang = "en";
+        else if (win_loc.includes("www.wikipedia."))
+            var lang = win_loc.split('/')[2].split('.')[2];
+        else if (win_loc.includes(".wikipedia.org"))
+            var lang = win_loc.split('/')[2].split('.')[0];
+        else
+            return;
+        win_loc = win_loc.replace(/^.*\.wikipedia\.[a-zA-Z]*\//, 'https://wikiless.org/') + "?lang=" + lang;
+    } else if (win_loc.includes("imgur")) {
+        win_loc =win_loc.replace('https://imgur.com','https://rimgo.bcow.xyz');
+    } else if (win_loc.includes("imdb")) {
+        win_loc = win_loc.replace(/^.*\.?imdb\.com\//, 'http://libremdb.lqs5fjmajyp7rvp4qvyubwofzi6d4imua7vs237rkc4m5qogitqwrgyd.onion/');
+    } else if (win_loc.includes("quora")) {
+        win_loc = win_loc.replace(/^.*quora\.[a-zA-Z]*\//, 'https://quetre.herokuapp.com/');
+    } else if (win_loc.includes("reuters")) {
+        win_loc = win_loc.replace(/^.*reuters\.[a-zA-Z]*\//, 'https://neuters.de/');
+    } else {
+        return;
+    }
+    window.location = win_loc;
 })();
 /* Add this code as a bookmark URL */
